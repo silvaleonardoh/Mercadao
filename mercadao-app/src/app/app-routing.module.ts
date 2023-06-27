c95bf2a625f3b69c7a6bcd68dc4c5289f552ab41
Leonardo Oliveira <nprefiro@gmail.com>
2023-06-27 12:56:41 -0300

    aprimoramentos UI

--- mercadao-app/src/app/app-routing.module.ts
+++ mercadao-app/src/app/app-routing.module.ts
@@ -1,26 +1,34 @@
-import { HomeComponent } from './pages/home/home.component';
+
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { EstatisticasComponent } from './pages/estatisticas/estatisticas.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { TiComponent } from './pages/estatisticas/areas/ti/ti.component';
 
 const routes: Routes = [
     {
-        path: '', component: HomeComponent
+        path: '', 
+        component: HomeComponent
     },
     {
-        path: 'estatisticas', component: EstatisticasComponent
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
     },
     {
-        path: 'sobre', component: SobreComponent
+        path: 'estatisticas',
+        component: EstatisticasComponent
     },
     {
-        path: 'ti', component: TiComponent
+        path: 'ti',
+        component: TiComponent
     },
     {
-        path: '', redirectTo: '/home', pathMatch: 'full'
-    }
+        path: 'sobre',
+        component: SobreComponent
+    },
+    
 ];
 
 @NgModule({
--- mercadao-app/src/app/app.module.ts
+++ mercadao-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
@@ -8,7 +9,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { EstatisticasComponent } from './pages/estatisticas/estatisticas.component';
 import { TiComponent } from './pages/estatisticas/areas/ti/ti.component';
-import { RouterModule } from '@angular/router';
 
 
 @NgModule({
